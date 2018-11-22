3400db762ee52c2613cffcab9c8ef4a0f2733a2d
Polova <booozyby@gmail.com>
2018-11-22 17:44:14 +0200

    Initial new project

---
+++ src/app/main/main.controller.js
@@ -0,0 +1,41 @@
+export class MainController {
+  constructor ($timeout, webDevTec, toastr) {
+    'ngInject';
+
+    this.awesomeThings = [];
+    this.classAnimation = '';
+    this.creationDate = 1542888925003;
+    this.toastr = toastr;
+
+    this.myfirstsService = []
+
+    this.activate($timeout, webDevTec);
+  }
+
+  activate($timeout, webDevTec) {
+    this.getWebDevTec(webDevTec);
+    $timeout(() => {
+      this.classAnimation = 'rubberBand';
+    }, 4000);
+    this.getMydata(webDevTec);
+  }
+
+  getWebDevTec(webDevTec) {
+    this.awesomeThings = webDevTec.getTec();
+
+    angular.forEach(this.awesomeThings, (awesomeThing) => {
+      awesomeThing.rank = Math.random();
+    });
+  }
+  getMydata(webDevTec){
+    this.myfirstsService = webDevTec.getYesNodata();
+    angular.forEach(this.myfirstsService, (cont) => {
+      cont.rank = Math.random();
+    })
+  }
+
+  showToastr() {
+    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+    this.classAnimation = '';
+  }
+}
